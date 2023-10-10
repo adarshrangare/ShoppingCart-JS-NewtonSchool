0bf5908bd94df6a9269a971491cc4437a1008958
Adarsh Rangare <akrangare@gmail.com>
2023-10-10 15:34:46 +0530

    Update Script-Section-2.js

--- Script-Section-2.js
+++ Script-Section-2.js
@@ -1,1 +1,20 @@
 
+
+    // Section 2
+
+    const cartIcon = document.querySelector(".carticon");
+    const cartUI = document.querySelector(".cartui");
+
+    cartIcon.addEventListener("click",(e)=>{
+        console.log("clicked Cart")
+       cartUI.classList.add("cartopened");
+        
+    })
+
+    const closeCart = document.querySelector(".closecart");
+
+    closeCart.addEventListener("click",(e)=>{
+        console.log("clicked Cart")
+       cartUI.classList.remove("cartopened");
+        
+    })
