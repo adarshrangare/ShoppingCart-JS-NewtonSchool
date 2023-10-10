7a8b0b47300216746b34e9a87c44db3fb6e22aca
adarshrangare <akrangare@gmail.com>
2023-10-10 16:06:51 +0530

    added4

--- Script-Section-4.js
+++ Script-Section-4.js
@@ -1,1 +1,25 @@
 
+class Storage {
+    
+    static getproducts() {
+      let products = [];
+      if (localStorage.getItem("products")) {
+        products = JSON.parse(localStorage.getItem("products"));
+      }
+      return products;
+    }
+  
+    
+    static addtolocalstorage(product) {
+      const products = this.getproducts();
+      products.push(product);
+      localStorage.setItem("products", JSON.stringify(products));
+    }
+  
+    
+    static removeproduct(id) {
+      const products = this.getproducts();
+      const updatedProducts = products.filter((product) => product.id !== id);
+      localStorage.setItem("products", JSON.stringify(updatedProducts));
+    }
+  }
